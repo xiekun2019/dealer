ab709af7d6e26061b99d19089b7d59b60f2feff1
xiekun <xiekun@xiaomi.com>
2023-01-29 23:34:51 +0800

    引入日志框架 logback，使用 Slf4j 打印日志

--- dealer-web/src/main/java/com/xiekun/dealer/web/TestController.java
+++ dealer-web/src/main/java/com/xiekun/dealer/web/TestController.java
@@ -1,13 +1,16 @@
 package com.xiekun.dealer.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class TestController {
 
     @RequestMapping("/test")
     private String test() {
+        log.info("你好呀，罗蕾");
         return "Dealer test";
     }
 }
