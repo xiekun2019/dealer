a6b0d362f2af8130893796aac62a864bedef82cc
xiekun <xiekun@xiaomi.com>
2023-02-12 15:48:21 +0800

    AssembleAction 实现(2)

--- dealer-service-impl/src/main/java/com/xiekun/dealer/service/impl/action/AssembleAction.java
+++ dealer-service-impl/src/main/java/com/xiekun/dealer/service/impl/action/AssembleAction.java
@@ -5,6 +5,7 @@ import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONUtil;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.google.common.base.Throwables;
 import com.xiekun.dealer.common.constant.CommonConstant;
 import com.xiekun.dealer.common.domain.TaskInfo;
 import com.xiekun.dealer.common.dto.model.ContentModel;
@@ -18,15 +19,21 @@ import com.xiekun.dealer.support.dao.MessageTemplateDao;
 import com.xiekun.dealer.support.domain.MessageTemplate;
 import com.xiekun.dealer.support.pipeline.BusinessProcess;
 import com.xiekun.dealer.support.pipeline.ProcessContext;
+import com.xiekun.dealer.support.utils.ContentHolderUtil;
 import com.xiekun.dealer.support.utils.TaskInfoUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.lang.reflect.Field;
 import java.util.*;
 
+@Slf4j
 @Service
 public class AssembleAction implements BusinessProcess<SendTaskModel> {
+
+    private static final String LINK_NAME = "url";
+
     @Autowired
     private MessageTemplateDao messageTemplateDao;
 
@@ -34,16 +41,22 @@ public class AssembleAction implements BusinessProcess<SendTaskModel> {
     public void process(ProcessContext<SendTaskModel> context) {
         SendTaskModel sendTaskModel = context.getProcessModel();
         Long messageTemplateId = sendTaskModel.getMessageTemplateId();
-
-        // 查询数据库，看是否有这个 messageTemplate
-        Optional<MessageTemplate> messageTemplate = messageTemplateDao.findById(messageTemplateId);
-        if (messageTemplate.isEmpty() || messageTemplate.get().getIsDeleted().equals(CommonConstant.TRUE)) {
-            context.setNeedBreak(true).setResponse(BasicResultVO.fail(RespStatusEnum.TEMPLATE_NOT_FOUND));
-            return;
-        }
-
-        if (BusinessCode.COMMON_SEND.getCode().equals(context.getCode())) {
-
+        try {
+            // 查询数据库，看是否有这个 messageTemplate
+            Optional<MessageTemplate> messageTemplate = messageTemplateDao.findById(messageTemplateId);
+            if (messageTemplate.isEmpty() || messageTemplate.get().getIsDeleted().equals(CommonConstant.TRUE)) {
+                context.setNeedBreak(true).setResponse(BasicResultVO.fail(RespStatusEnum.TEMPLATE_NOT_FOUND));
+                return;
+            }
+            if (BusinessCode.COMMON_SEND.getCode().equals(context.getCode())) {
+                List<TaskInfo> taskInfos = assembleTaskInfo(sendTaskModel, messageTemplate.get());
+                sendTaskModel.setTaskInfo(taskInfos);
+            } else if (BusinessCode.RECALL.getCode().equals(context.getCode())) {
+                sendTaskModel.setMessageTemplate(messageTemplate.get());
+            }
+        } catch (Exception e) {
+            context.setNeedBreak(true).setResponse(BasicResultVO.fail(RespStatusEnum.SERVICE_ERROR));
+            log.error("assemble task fail! templateId:{}, e:{}", messageTemplateId, Throwables.getStackTraceAsString(e));
         }
     }
 
@@ -78,18 +91,17 @@ public class AssembleAction implements BusinessProcess<SendTaskModel> {
     }
 
     /**
-     * 获取 contentModel，替换模板msgContent中占位符信息
+     * 获取 contentModel，替换模板 msgContent 中占位符信息
      */
     private static ContentModel getContentModelValue(MessageTemplate messageTemplate, MessageParam messageParam) {
 
-        // 得到真正的ContentModel 类型
+        // 得到真正的 ContentModel 类型
         Integer sendChannel = messageTemplate.getSendChannel();
         Class<? extends ContentModel> contentModelClass = ChannelType.getChanelModelClassByCode(sendChannel);
 
-        // 得到模板的 msgContent 和 入参
+        // 得到模板的 msgContent 和入参
         Map<String, String> variables = messageParam.getVariables();
         JSONObject jsonObject = JSON.parseObject(messageTemplate.getMsgContent());
-
 
         // 通过反射 组装出 contentModel
         Field[] fields = ReflectUtil.getFields(contentModelClass);
@@ -104,7 +116,7 @@ public class AssembleAction implements BusinessProcess<SendTaskModel> {
             }
         }
 
-        // 如果 url 字段存在，则在url拼接对应的埋点参数
+        // 如果 url 字段存在，则在 url 拼接对应的埋点参数
         String url = (String) ReflectUtil.getFieldValue(contentModel, LINK_NAME);
         if (StrUtil.isNotBlank(url)) {
             String resultUrl = TaskInfoUtils.generateUrl(url, messageTemplate.getId(), messageTemplate.getTemplateType());
