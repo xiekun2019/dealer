1b08adb3f9bbf05bbdfb5a31ec61c4cee7ea45e7
xiekun <xiekun@xiaomi.com>
2023-02-16 23:48:37 +0800

    spring 接入 kafka，并且实现 KafkaTemplate 发送消息

---
+++ dealer-support/src/main/java/com/xiekun/dealer/support/constant/MessageQueuePipeline.java
@@ -0,0 +1,13 @@
+package com.xiekun.dealer.support.constant;
+
+/**
+ * 消息队列常量
+ *
+ * @author 3y
+ */
+public interface MessageQueuePipeline {
+    String EVENT_BUS = "eventBus";
+    String KAFKA = "kafka";
+    String ROCKET_MQ = "rocketMq";
+    String RABBIT_MQ = "rabbitMq";
+}
---
+++ dealer-support/src/main/java/com/xiekun/dealer/support/mq/kafka/KafkaSendMqServiceImpl.java
@@ -0,0 +1,45 @@
+package com.xiekun.dealer.support.mq.kafka;
+
+import cn.hutool.core.util.StrUtil;
+import com.xiekun.dealer.support.constant.MessageQueuePipeline;
+import com.xiekun.dealer.support.mq.SendMqService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.header.Header;
+import org.apache.kafka.common.header.internals.RecordHeader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+
+@Slf4j
+@Service
+@ConditionalOnProperty(name = "austin.mq.pipeline", havingValue = MessageQueuePipeline.KAFKA)
+public class KafkaSendMqServiceImpl implements SendMqService {
+
+    @Autowired
+    private KafkaTemplate kafkaTemplate;
+
+    @Value("${dealer.business.tagId.key}")
+    private String tagIdKey;
+
+    @Override
+    public void send(String topic, String jsonValue, String tagId) {
+        if (StrUtil.isNotBlank(tagId)) {
+            List<Header> headers = Arrays.asList(new RecordHeader(tagIdKey, tagId.getBytes(StandardCharsets.UTF_8)));
+            kafkaTemplate.send(new ProducerRecord(topic, null, null, null, jsonValue, headers));
+        } else {
+            kafkaTemplate.send(topic, jsonValue);
+        }
+    }
+
+    @Override
+    public void send(String topic, String jsonValue) {
+        send(topic, jsonValue, null);
+    }
+}
