7d602e1b2095df6c401a41eecb36ceaecc122d05
xiekun2019 <xiekun2019@gmail.com>
2023-03-05 15:11:41 +0800

    实现去重(3)：实现 ContentDeduplicationService 内容去重

---
+++ dealer-handler/src/main/java/com/xiekun/dealer/handler/handler/deduplication/service/ContentDeduplicationService.java
@@ -0,0 +1,21 @@
+package com.xiekun.dealer.handler.handler.deduplication.service;
+
+import cn.hutool.crypto.digest.DigestUtil;
+import com.alibaba.fastjson.JSON;
+import com.xiekun.dealer.common.domain.TaskInfo;
+
+public class ContentDeduplicationService extends AbstractDeduplicationService {
+
+    /**
+     * 构建内容去重 key
+     * <p>
+     * key：md5(templateId + receiver + content)
+     * <p>
+     * 相同的内容相同的模板，在短时间内只发给同一个人
+     */
+    @Override
+    public String deduplicationSingleKey(TaskInfo taskInfo, String receiver) {
+        return DigestUtil.md5Hex(taskInfo.getMessageTemplateId() + receiver
+                + JSON.toJSONString(taskInfo.getContentModel()));
+    }
+}
